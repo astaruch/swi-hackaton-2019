5de1f8f9a778dede7dcacb5d4afe094b6241a9f9
Andrej Staruch <andrej.staruch@solarwinds.com>
2019-05-31 09:10:43 +0200

    Rewriten server

--- html/server.js
+++ html/server.js
@@ -1,20 +1,41 @@
-console.log('Server started')
-var WebSocketServer = require('ws').Server
-var wss = new WebSocketServer({ port: 9999 })
+'use strict'
+const express = require('express')
+const WebSocketServer = require('ws').Server
+const ip = require('ip')
+// const uuid = require('uuid')
 
-var screenSharer
-var screenController
+const httpServer = express()
+const httpServerPort = process.env.PORT || 8080
 
-wss.on('connection', function (ws) {
-  if (!screenSharer) {
-    console.log('Connected screen sharer')
-    screenSharer = ws
+httpServer.use(express.static(__dirname))
+httpServer.listen(httpServerPort)
+console.dir(`Available on: http://${ip.address()}:${httpServerPort}`)
+
+const wsPort = process.env.WSPORT || 9999
+const wss = new WebSocketServer({
+  port: wsPort
+  // verifyClient: (info, done) => {
+  //   info.req.session.userId = uuid.v4()
+  //   console.log(`Adding id ${info.req.session.userId} to user`)
+  //   done(info.req.session.userId)
+  // }
+})
+console.log(`WebSocket server is listening on port ${wsPort}`)
+
+let master
+let clients = []
+
+wss.on('connection', (ws, req) => {
+  // console.log(`Client ${req.session.userId} connected`)
+  if (!master) {
+    console.log('Connected master')
+    master = ws
   } else {
-    console.log('Connected screen controller')
-    screenController = ws
-    screenController.on('message', function (message) {
-      console.log(`Sending mouse position: ${message}`)
-      screenSharer.send(message)
+    console.log('Connected client')
+    clients.push(ws)
+    ws.on('message', message => {
+      console.log(`Sending: ${message}`)
+      master.send(message)
     })
   }
 })
